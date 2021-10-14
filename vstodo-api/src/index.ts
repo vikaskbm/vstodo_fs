4a362d3008f354dc4f3ea5985bca64fd7e894855
Vikas Bishnoi <vikaskbm@gmail.com>
2021-10-15 00:46:56 +0530

    setup passport and github auth

--- vstodo-api/src/index.ts
+++ vstodo-api/src/index.ts
@@ -1,10 +1,12 @@
 import "reflect-metadata";
+require('dotenv-safe').config();
 import express from "express";
 import { createConnection } from "typeorm";
 import { __prod__ } from "./constants";
 import { join } from "path";
 import { User } from "./entities/User";
-
+import { Strategy as GitHubStrategy } from "passport-github";
+import passport from 'passport';
 
 // (async () => {
 //     const app = express();
@@ -22,10 +24,42 @@ const main = async () => {
         synchronize: !__prod__
     })
 
-    const user = await User.create({name: 'mukesh'}).save();
-    console.log(user)
+    // const user = await User.create({name: 'mukesh'}).save();
+    // console.log(user)
+
     const app = express();
-    app.get('/aa', (_req, res) => {
+    passport.serializeUser(function(user: any, done) {
+        done(null, user.accessToken);
+    });
+    app.use(passport.initialize());
+
+    passport.use(
+        new GitHubStrategy(
+            {
+                clientID: process.env.GITHUB_CLIENT_ID,
+                clientSecret: process.env.GITHUB_CLIENT_SECRET,
+                callbackURL: "http://localhost:3002/auth/github/callback",
+            },
+            (_, __, profile, cb) => {
+                console.log(profile)
+                cb(null, {accessToken: '', refreshToken: ''})
+            }
+        )
+    );
+
+    app.get('/auth/github', passport.authenticate('github', {session: false}));
+
+    app.get(
+        '/auth/github/callback', 
+        passport.authenticate('github', {session: false}),
+        function(_req, res) {
+            // Successful authentication, redirect home.
+            res.send('you logged in correctly');
+        }
+    );
+
+    // APIs
+    app.get('/', (_req, res) => {
         res.send({
             name: "hello",
             age: 22
