bd2302c2ed773d0f54981dbc41d035366b782805
Vikas Bishnoi <vikaskbm@gmail.com>
2021-10-14 23:57:46 +0530

    Connect postgresql with with application

--- vstodo-api/src/index.ts
+++ vstodo-api/src/index.ts
@@ -1,4 +1,9 @@
+import "reflect-metadata";
 import express from "express";
+import { createConnection } from "typeorm";
+import { __prod__ } from "./constants";
+import { join } from "path";
+import { User } from "./entities/User";
 
 
 // (async () => {
@@ -7,9 +12,24 @@ import express from "express";
 
 
 const main = async () => {
+    const conn = await createConnection({
+        type: 'postgres',
+        database: 'vstodo',
+        username: 'vstodo_user',
+        password: 'Vikas@029',
+        entities: [join(__dirname, './entities/*.*')],
+        logging: !__prod__,
+        synchronize: !__prod__
+    })
+
+    const user = await User.create({name: 'mukesh'}).save();
+    console.log(user)
     const app = express();
-    app.get('/', (_req, res) => {
-        res.send("hello")
+    app.get('/aa', (_req, res) => {
+        res.send({
+            name: "hello",
+            age: 22
+        })
     })
     app.listen(3002, ()=> {
         console.log("Listening on 3002")
